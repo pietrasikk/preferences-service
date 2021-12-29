dc7e6b4b6cf51e24252881f83a99aa6a2309f459
Kacper <pietrasikk@gmail.com>
2021-12-29 21:24:51 +0100

    Init project

---
+++ src/test/java/com/manning/tutorial/notification/NotificationPreferencesService/NotificationPreferencesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.tutorial.notification.NotificationPreferencesService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationPreferencesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
